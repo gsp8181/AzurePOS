4b806c4393c7ba6dfa1eba62198b1417ba4e96d8
gsp8181 <gsp8181@users.noreply.github.com>
2014-12-02 20:42:09 +0000

    Started the structure of the command line

---
+++ AzurePOS/CommandLineOptions.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace AzurePOS
+{
+    class Options
+    {
+        //[Option('r', "read", Required = true,
+        //  HelpText = "Input file to be processed.")]
+        //public string InputFile { get; set; }
+
+        // omitting long name, default --verbose
+        /*[Option('g', "gui",
+          HelpText = "Displays the GUI associated with the program", MutuallyExclusiveSet = "gui")]
+        public bool gui { get; set; }
+
+        [Option('t', "type", HelpText = "Database to perform operations on (customer/order)")]
+        public Type type { get; set; }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }*/
+
+        public Options()
+        {
+           // ListVerb = new ListSubOptions { Patch = true };
+        }
+        [VerbOption("list", HelpText = "List a specified database (customer|order).")]
+        public ListSubOptions ListVerb { get; set; }
+
+        [VerbOption("register", HelpText = "Register a new customer.")]
+        public RegisterSubOptions RegisterVerb { get; set; }
+
+        [VerbOption("order", HelpText = "Register a new order.")]
+        public OrderSubOptions OrderVerb { get; set; }
+
+        [VerbOption("location", HelpText = "View (or set) the location of this instance.")]
+        public LocationSubOptions LocationCountryVerb { get; set; }
+
+        [HelpVerbOption]
+        public string GetUsage(string verb)
+        {
+            return HelpText.AutoBuild(this, verb);
+        }
+
+    }
+
+    class ListSubOptions
+    {
+        [Option('t', "type", HelpText = "Database to perform operations on (customer/order)", Required=true)]
+        public database type { get; set; }
+
+
+    }
+
+    class RegisterSubOptions
+    {
+        // Remainder omitted
+    }
+
+    class OrderSubOptions
+    {
+        // Remainder omitted 
+    }
+
+    class LocationSubOptions
+    {
+        // Remainder omitted 
+    }
+
+    enum database
+    {
+        customer,
+        order
+    }
+
+}
--- AzurePOS/Program.cs
+++ AzurePOS/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CommandLine;
 
 namespace AzurePOS
 {
@@ -12,11 +13,56 @@ namespace AzurePOS
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            string invokedVerb = "";
+            object invokedVerbInstance = null;
+
+            var options = new Options();
+            if (!CommandLine.Parser.Default.ParseArguments(args, options,
+              (verb, subOptions) =>
+              {
+                  // if parsing succeeds the verb name and correct instance
+                  // will be passed to onVerbCommand delegate (string,object)
+                  invokedVerb = verb;
+                  invokedVerbInstance = subOptions;
+              }))
+            {
+                Environment.Exit(CommandLine.Parser.DefaultExitCodeFail);
+            }
+
+            if (invokedVerb == "list")
+            {
+                ListSubOptions commitSubOptions = (ListSubOptions)invokedVerbInstance;
+                if(commitSubOptions.type == database.customer)
+                {
+
+                } 
+                else if (commitSubOptions.type == database.order)
+                {
+
+                }
+            
+            }
+            if (invokedVerb == "register")
+            {
+                RegisterSubOptions commitSubOptions = (RegisterSubOptions)invokedVerbInstance;
+            }
+            if (invokedVerb == "order")
+            {
+                OrderSubOptions commitSubOptions = (OrderSubOptions)invokedVerbInstance;
+            }
+            if (invokedVerb == "location")
+            {
+                LocationSubOptions commitSubOptions = (LocationSubOptions)invokedVerbInstance;
+            }
+        }
+
+        private static void RunGUI()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
         }
     }
 }
