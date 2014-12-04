94e0a86107830c4dbd4c65062ce1af3612f0b240
gsp8181 <gsp8181@users.noreply.github.com>
2014-12-04 14:07:33 +0000

    Changed report to a console application

--- Report/Program.cs
+++ Report/Program.cs
@@ -1,22 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Report
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
         }
     }
 }
